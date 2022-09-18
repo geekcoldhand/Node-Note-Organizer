c00a73e052fd1a1d0bb0c850ec5bc4c1ee6a5574
git geek <cout.1.endl@gmail.com>
2022-09-18 19:24:35 -0400

    created api structure

--- server/server.js
+++ server/server.js
@@ -1,19 +1,31 @@
 const express = require("express");
 const path = require("path");
+// const apiRoutes = require('./routes/apiRoutes')
 
 const app = express();
 const PORT = process.env.port || 5502;
 
-app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("../public"));
+// app.use('/api', apiRoutes);
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 app.get("/notes", (req, res) => {
-  console.log("here");
   res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
+
+// endpoint  - GET request - /api/notes
+// ** send them json
+// -- look into your db - read a file
+// -- convert it if needed
+// -- res.json() that data back to the users
+app.get("/api/notes", (req, res) => {});
+app.post("/api/notes", (req, res) => {});
+app.delete("/api/notes", (req, res) => {});
 
 app.listen(PORT, () => {
   console.log(`listening at http://localhost:${PORT}`);
